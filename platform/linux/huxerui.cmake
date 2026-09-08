108db9c4a97bbd512ac5a3f21676b2677fde2e10
FarnaHerry <farnaherry@gmail.com>
2026-09-08 14:29:25 +0800

    feat(package): publish Linux deb and rpm artifacts

---
+++ platform/linux/huxerui.cmake
@@ -0,0 +1,30 @@
+function(huxerui_configure_linux_project_package target_name install_component)
+    if (NOT HUXERUI_PACKAGE)
+        return()
+    endif ()
+
+    # CPack uses /usr as the package prefix. Keep the executable and resource
+    # package in the conventional Linux application locations.
+    install(TARGETS ${target_name}
+            RUNTIME DESTINATION bin
+            COMPONENT "${install_component}"
+    )
+    _huxerui_install_runtime_dependencies(${target_name} "${install_component}"
+            . "bin/$<TARGET_FILE_NAME:${target_name}>"
+    )
+    get_target_property(HUXERUI_LINUX_APP_RESOURCES
+            ${target_name}
+            HUXERUI_RESOURCE_PACKAGE
+    )
+    if (HUXERUI_LINUX_APP_RESOURCES
+            AND NOT HUXERUI_LINUX_APP_RESOURCES MATCHES "-NOTFOUND$")
+        install(DIRECTORY "${HUXERUI_LINUX_APP_RESOURCES}/"
+                DESTINATION "bin/${target_name}.resources"
+                COMPONENT "${install_component}"
+        )
+    endif ()
+    install(DIRECTORY "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../assets"
+            DESTINATION bin
+            COMPONENT "${install_component}"
+    )
+endfunction()
